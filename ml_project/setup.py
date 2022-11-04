9ec40acebf3ee81cd38b8be556bf3deb8cc66ccc
aitkachenko <aitkachenko@phoenixit.ru>
2022-11-04 22:59:43 +0300

    init commit

---
+++ ml_project/setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='src',
+    packages=find_packages(),
+    version='0.1.0',
+    description='project for homework #01 mlops course',
+    author='aitk@data.vk.company[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[Drt.tjackachenko@data.vk.company',
+    license='MIT',
+)
