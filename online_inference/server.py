727f8154290d56fe9318b1cfeec3eeac93bd8434
aitkachenko <aitkachenko@phoenixit.ru>
2022-12-21 23:39:39 +0300

    init homework #4

--- online_inference/server.py
+++ online_inference/server.py
@@ -9,6 +9,9 @@ import pandas as pd
 
 from typing import Literal
 
+import time
+start_time = time.time()
+
 app = FastAPI()
 model = None
 transformer = None
@@ -66,9 +69,21 @@ def predict(request: MedicalData):
 def root():
     return {"message": "service is up"}
 
+@app.get("/ready", status_code=200)
+def ready() -> bool:
+    cur_time = time.time()
+    if cur_time - start_time < 30.0:
+        return false
+    #return (model is not None) and (transformer is not None)
+    return true
+
 @app.get("/health", status_code=200)
 def health() -> bool:
-    return (model is not None) and (transformer is not None)
+    cur_time = time.time()
+    if cur_time - start_time < 35.0 or cur_time - start_time > 120.0:
+        return false
+    #return (model is not None) and (transformer is not None)
+    return true
 
 if __name__ == "__main__":
     uvicorn.run("server:app", host="0.0.0.0", port=os.getenv("PORT", 18000))
